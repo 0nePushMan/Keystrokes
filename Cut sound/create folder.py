ba30f2f1a48e9d4f9394b526d761aedd9f2e5fc4
Nairi <tnairi06@outlook.fr>
2022-03-09 09:42:38 +0100

    [-] Remove create folder & cut_sound

--- Cut sound/create folder.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-alphabet = ['a', 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', "'", ' ', 'backspace']
-
-for i in alphabet: 
-    try:
-        os.makedirs('./Keystrokes/' + i)
-    except:
-        print(i + ' Already exist donc tg')
